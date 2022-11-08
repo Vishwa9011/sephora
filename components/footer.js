f8e120e76f58f3d60f3e597137c82d0347f607d9
Jimmy-Sharma <prince786no.1@gmail.com>
2022-11-08 19:14:04 +0530

    footer

---
+++ components/footer.js
@@ -0,0 +1,237 @@
+const footer=()=>
+{
+    return `
+    <div id="top">
+            <h1>GET THE LATEST NEWS & OFFERS IN BEAUTY & FASHION</h1>
+            <div id="top2">
+                <div id="contact">
+                    <input class="contact" type="email" placeholder="Enter your email address">
+                    <button class="contact" id="subscribe">SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
+        <div id="middle">
+            <div>
+                <h1>Sephora online India is a trailblazer in the Indian beauty scene</h1>
+                <p>A leader in luxury beauty and skincare, our purpose at Sephora online India is to create an inviting
+                    online beauty shopping experience and inspire fearlessness in our community. Since the Sephora India
+                    launch in 2013, we have been an industry-leading champion of diversity, and empowerment, guided by
+                    our longstanding company values.</p>
+                <h2>Discover who we are and what we entail only on NNNOW.com</h2>
+                <p>Owned by LVMH Moet Hennessy Louis Vuitton, the world's leading luxury goods group, Sephora was
+                    founded in France by Dominique Mandonnaud in 1970 and is defined by its unique, open-sell
+                    environment with an ever-increasing assortment of best-selling lipsticks, eyeliners, foundations,
+                    eye palettes and more from carefully curated brands such as Make Up For Ever, Estee Lauder, Benefit
+                    Cosmetics, Huda Beauty and many more.</p>
+                <h2>Shop Our unrivalled range of Sephora products for a more beautiful you</h2>
+                <p>Our array on Sephora NNNOW features trusted classics from Pixi, Elizabeth Arden, Anastasia Beverly
+                    Hills, Clinique, Tom Ford, and Sephora’s own, SEPHORA COLLECTION. Today, Sephora India online is a
+                    powerful beauty presence in the digital space, thanks to its unparalleled assortment of prestige
+                    products in every category, unbiased service from beauty experts, interactive shopping environment,
+                    and innovation.
+                    <br>
+                    <br>
+
+                    Through some of our bestsellers such as Sephora lipsticks, Sephora foundations, and other Sephora
+                    products, we explore the universe of beauty and wellness with an unbiased approach to experiential
+                    retail. We also host expert tutorial videos from our top brands to help you nail the perfect smokey
+                    eye or winged eyeliner. Our team of experts in-store also works hard to assure you have a seamless
+                    shopping experience with us and helps you define your skin undertones while purchasing the right
+                    sephora foundation for you. Sephora continues to give back to communities and advance inclusion in
+                    the beauty industry by providing an unrivaled range of shades for Sephora foundations at unbeatable
+                    prices.
+                    <br>
+                    <br>
+
+                    Sephora in India also encompasses a formidable range of skincare, bath and body products, haircare,
+                    fragrances and perfumes from international brands. Many of our brands such as Botanist and Forest
+                    Essentials boast of incorporating only natural ingredients in their products, whereas award-winning
+                    such as Natasha Denona, Nudestix, Kora Organics, Aveda, Olaplex, and more promise only
+                    SLS/Paraben-free formulas in their range of products. Be sure to also check out our range of
+                    cruelty-free brands that include best-selling brands such as Caudalie, Smashbox, Dear Dahlia, Juice
+                    Beauty and much more. Sephora online hosts the entire range of makeup and skin products available in
+                    Sephora stores in India. Buy Sephora online on NNNOW and be the first to be alerted when we have a
+                    major Sephora India sale event coming up!
+                    <br>
+                    <br>
+
+                    Start your Sephora India online shopping on NNNOW.com
+                    <br>
+                    <br>
+
+                    Women can now shop their favorite luxury beauty brands on the Sephora site or via the NNNOW mobile
+                    app. Be spoilt for choice with a wide variety of makeup, skincare, haircare, bath and body and
+                    fragrances that you can shop from the comfort of your home. Discover online shopping that offers
+                    advantages such as Gift With Purchase, freebie sachets, and discount offers on beauty! Buy Sephora
+                    online at amazing deals and prices at the click of a button.
+                </p>
+            </div>
+            <div id="faq">
+                <a class="change" href="">WHO WE ARE</a>
+                <a href="">FREQUENTLY ASKED QUESTIONS</a>
+                <a href="">CONTACT US</a>
+            </div>
+            <div id="socialMedia">
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+            <div id="about">
+                <a href="">@ 2022 NNNOW</a>
+                <span> | </span>
+                <a href="">Privacy Policy</a>
+                <span> | </span>
+                <a href="">Terms & Conditions</a>
+            </div>
+            <div class="bottomDown" id="brands">
+                <h2>Brands</h2>
+                <div class="bottom">
+                    <a href="">Sephora Collection</a>
+                    <span> | </span>
+                    <a href="">Benefit Cosmetics</a>
+                    <span> | </span>
+                    <a href="">Make Up For Ever</a>
+                    <span> | </span>
+                    <a href="">Cover FX</a>
+                    <span> | </span>
+                    <a href="">Stila</a>
+                    <span> | </span>
+                    <a href="">Natio</a>
+                    <span> | </span>
+                    <a href="">Burt's Bees</a>
+                    <span> | </span>
+                    <a href="">Foreo</a>
+                    <span> | </span>
+                    <a href="">Smashbox</a>
+                    <span> | </span>
+                    <a href="">Glamglow</a>
+                    <span> | </span>
+                    <a href="">Clinique</a>
+                    <span> | </span>
+                    <a href="">Estee Lauder</a>
+                    <span> | </span>
+                    <a href="">Elizabeth Arden</a>
+                    <span> | </span>
+                </div>
+                <div class="bottom">
+                    <a href="">Givenchy</a>
+                    <span> | </span>
+                    <a href="">Bobbi Brown</a>
+                    <span> | </span>
+                    <a href="">Shiseido</a>
+                    <span> | </span>
+                    <a href="">OPI</a>
+                    <span> | </span>
+                    <a href="">Forest Essentials</a>
+                    <span> | </span>
+                    <a href="">Percy & Reed</a>
+                    <span> | </span>
+                    <a href="">Boscia</a>
+                    <span> | </span>
+                    <a href="">Oscar Blandi</a>
+                    <span> | </span>
+                    <a href="">Burberry</a>
+                    <span> | </span>
+                    <a href="">CK Frangrances</a>
+                    <span> | </span>
+                    <a href="">Kenzo</a>
+                    <span> | </span>
+                    <a href="">Tom Ford</a>
+                    <span> | </span>
+                    <a href="">Bvlgari</a>
+                    <span> | </span>
+                    <a href="">Carolina Herrera</a>
+                    <span> | </span>
+                </div>
+                <div class="bottom">
+                    <a href="">Davidoff</a>
+                    <span> | </span>
+                    <a href="">Giorgio Armani</a>
+                    <span> | </span>
+                    <a href="">Montblanc</a>
+                    <span> | </span>
+                    <a href="">Paco Rabanne</a>
+                    <span> | </span>
+                    <a href="">Yves Saint Laurent</a>
+                    <span> | </span>
+                    <a href="">Azzaro</a>
+                    <span> | </span>
+                    <a href="">Nina Ricci</a>
+                    <span> | </span>
+                    <a href="">Roberto Cavalli</a>
+                    <span> | </span>
+                    <a href="">Zirh</a>
+                    <span> | </span>
+                    <a href="">Omorfee</a>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="bottomDown">
+            <h2>MAKEUP</h2>
+            <div class="bottom">
+                <a href="">Face</a>
+                <span> | </span>
+                <a href="">Cheek</a>
+                <span> | </span>
+                <a href="">Eye</a>
+                <span> | </span>
+                <a href="">Lip</a>
+                <span> | </span>
+                <a href="">Nail</a>
+                <span> | </span>
+                <a href="">Makeup Palettes</a>
+                <span> | </span>
+                <a href="">Accessories</a>
+            </div>
+
+        </div>
+        <div class="bottomDown">
+            <h2>SKIN CARE</h2>
+            <div class="bottom">
+                <a href="">Moisturizers</a>
+                <span> | </span>
+                <a href="">Cleansers</a>
+                <span> | </span>
+                <a href="">Treatments</a>
+                <span> | </span>
+                <a href="">Mask</a>
+                <span> | </span>
+                <a href="">Eye Care</a>
+                <span> | </span>
+                <a href="">Sun Care</a>
+                <span> | </span>
+                <a href="">Lip Treatments</a>
+            </div>
+
+        </div>
+        <div class="bottomDown">
+            <h2>OTHER CATEGORIES</h2>
+            <div class="bottom">
+                <a href="">Fragrances</a>
+                <span> | </span>
+                <a href="">Tools & Brushes</a>
+            </div>
+
+        </div>
+        <div class="bottomDown">
+            <h2>MY SEPHORA</h2>
+            <div class="bottom">
+                <a href="">My orders</a>
+                <span> | </span>
+                <a href="">Store locator</a>
+                <span> | </span>
+                <a href="">Sephora Facebook</a>
+                <span> | </span>
+                <a href=""> Sephora Instagram</a>
+            </div>
+
+        </div>
+
+    </div>
+`
+}
+
+export {footer}
